2c49e0c75aa317b21880415218a3bc6c8362a7b4
Radheshyam gupta <radheshyamjio52@gmail.com>
2024-02-14 14:04:37 +0530

    route and page completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home"
+import Profile from "./pages/Profile";
+import Signing from "./pages/Signing";
+import About from "./pages/About";
+import Signout from "./pages/Signout";
 function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/sign-in" element={<Signing/>}/>
+        <Route path="/sign-out" element={<Signout/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Signing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Signing() {
+  return (
+    <div>Signing</div>
+  )
+}
+
+export default Signing
